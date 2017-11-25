db71bfb505620888047cd297efaf55b792961e46
V.Payonk <v.payonk@gmail.com>
2017-11-25 20:43:52 +0200

    init project

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+ 	return gulp.src('project/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('project/css'))
+	.pipe(browserSync.reload({
+	stream: true
+	}))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  	gulp.watch('project/scss/**/*.scss', ['sass']);
+  	gulp.watch('project/*.html', browserSync.reload); 
+  	gulp.watch('project/js/**/*.js', browserSync.reload);
+});
+
+
+gulp.task('browserSync', function() {
+	browserSync({
+ 		server: {
+ 		baseDir: 'project'
+ 		},
+  	})
+});
